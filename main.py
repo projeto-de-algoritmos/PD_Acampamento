47cfd135ae2d2181bc2f980f668b2bad2b63834c
lorenzo7377 <lorenzodavinci@hotmail.com>
2023-07-09 12:01:00 -0300

    Memorization

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-impor = []
-peso = []
-n = int(input("Peso da mochila: "))
+def knapsack(pesoMochila, pesoItens, impor, item):
+    memo = [[0 for i in range(pesoMochila + 1)] for j in range(item + 1)]
+    
+    for i in range (item):
+        for j in range (1, pesoMochila + 1):
+            if int(pesoItens[i]) > j:
+                memo[i+1][j] = memo[i][j]
+            else:
+                memo[i+1][j] = max(memo[i][j], int(impor[i]) + memo[i][j-int(pesoItens[i])])
+                
+    return memo
 
-for i in range(n):
+impor = []
+pesoItens = []
+pesoMochila = int(input("Peso da mochila: "))
+contador = int(input("quantidade de itens: "))
+
+for i in range(contador):
     valor = input(f"digite o peso do {i + 1}º item: ")
-    peso.append(valor)
+    pesoItens.append(valor)
     valor = input(f"digite a importância do {i + 1}º item: ")
     impor.append(valor)
 
-for i in range(n):
-    print(f"{peso[i]},{impor[i]}")
+
+m = knapsack(pesoMochila, pesoItens, impor, contador)
+
+for i in range(0, contador + 1):
+    for j in range(0, pesoMochila + 1):
+        print(f"[{m[i][j]}]", end='')
+    print("\n")
